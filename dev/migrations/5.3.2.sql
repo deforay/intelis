920786f0d0ceeb4b1b24d468315ce7a37d5a71f0
Amit Dugar <amit@deforay.com>
2025-09-22 18:29:07 +0530

    System Alerts Added - WIP

--- dev/migrations/5.3.2.sql
+++ dev/migrations/5.3.2.sql
@@ -4,3 +4,9 @@
 
 UPDATE `system_config` SET `value` = '5.3.2' WHERE `system_config`.`name` = 'sc_version';
 
+-- Amit 22-Sep-2024
+
+INSERT INTO `global_config`
+(`display_name`, `name`, `value`, `category`, `remote_sync_needed`, `updated_on`, `updated_by`, `status`)
+VALUES
+('Auto Approve Interface Results', 'auto_approve_interface_results', 'no', 'interfacing', 'yes', CURRENT_TIMESTAMP, NULL, 'active');
