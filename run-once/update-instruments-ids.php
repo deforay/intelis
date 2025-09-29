9f6639be444c4c914fda6069fd2455123d932a2e
Amit Dugar <amit@deforay.com>
2025-09-29 17:35:27 +0530

    Minor fixes in run-once scripts

--- run-once/update-instruments-ids.php
+++ run-once/update-instruments-ids.php
@@ -26,7 +26,7 @@ $scriptName = basename(__FILE__);
 
 // Check for force flag (-f or --force)
 $forceRun = in_array('-f', $argv) || in_array('--force', $argv);
-
+$scriptSucceeded = false;
 if (!$forceRun) {
     // Check if the script has already been run
     $db->where('script_name', $scriptName);
@@ -112,6 +112,7 @@ try {
     $db->setQueryOption('IGNORE')->insert('s_run_once_scripts_log', $data);
 
     echo "$scriptName executed and logged successfully" . PHP_EOL;
+    $scriptSucceeded = true;
 } catch (Throwable $e) {
     LoggerUtility::logError('Manifest hash update script failed', [
         'exception' => $e->getMessage(),
@@ -127,8 +128,4 @@ try {
             'status' => $scriptSucceeded ? 'executed' : 'forced'
         ]);
     }
-}
-
-if (!$scriptSucceeded) {
-    exit(1);
 }
