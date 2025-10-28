74b9827ac68d50ebbc77736df0708ea3255775f2
Amit Dugar <amit@deforay.com>
2025-10-28 13:47:54 +0530

    Code refactored across multiple files

--- app/system-admin/api-stats/getApiStatsDetails.php
+++ app/system-admin/api-stats/getApiStatsDetails.php
@@ -1,16 +1,20 @@
 <?php
 
-use App\Registries\ContainerRegistry;
+use App\Services\CommonService;
 use App\Services\DatabaseService;
+use App\Registries\ContainerRegistry;
 
 /** @var DatabaseService $db */
 $db = ContainerRegistry::get(DatabaseService::class);
+
+/** @var CommonService $general */
+$general = ContainerRegistry::get(CommonService::class);
 
 $tableName = "track_api_requests";
 $primaryKey = "api_track_id";
 
 
-$aColumns = array('requested_on', 'number_of_records', 'request_type', 'test_type', 'api_url', 'data_format');
+$aColumns = ['requested_on', 'number_of_records', 'request_type', 'test_type', 'api_url', 'data_format'];
 
 /* Indexed column (used for fast and accurate table cardinality) */
 $sIndexColumn = $primaryKey;
@@ -24,87 +28,59 @@ if (isset($_POST['iDisplayStart']) && $_POST['iDisplayLength'] != '-1') {
 }
 
 
+$sOrder = $general->generateDataTablesSorting($_POST, $orderColumns);
 
-$sOrder = "";
-if (isset($_POST['iSortCol_0'])) {
-    $sOrder = "";
-    for ($i = 0; $i < (int) $_POST['iSortingCols']; $i++) {
-        if ($_POST['bSortable_' . (int) $_POST['iSortCol_' . $i]] == "true") {
-            $sOrder .= $aColumns[(int) $_POST['iSortCol_' . $i]] . "
-                             " . ($_POST['sSortDir_' . $i]) . ", ";
-        }
-    }
-    $sOrder = substr_replace($sOrder, "", -2);
+$columnSearch = $general->multipleColumnSearch($_POST['sSearch'], $aColumns);
+$sWhere = [];
+if (!empty($columnSearch) && $columnSearch != '') {
+    $sWhere[] = $columnSearch;
 }
 
+$sQuery = "SELECT api.api_track_id,
+                api.requested_by,
+                api.requested_on,
+                api.number_of_records,
+                api.request_type,
+                api.test_type,
+                api.api_url,
+                api.api_params,
+                api.facility_id,
+                api.data_format 
+                FROM track_api_requests as api";
 
-$sWhere = "";
-if (isset($_POST['sSearch']) && $_POST['sSearch'] != "") {
-    $searchArray = explode(" ", (string) $_POST['sSearch']);
-    $sWhereSub = "";
-    foreach ($searchArray as $search) {
-        if ($sWhereSub == "") {
-            $sWhereSub .= "(";
-        } else {
-            $sWhereSub .= " AND (";
-        }
-        $colSize = count($aColumns);
-
-        for ($i = 0; $i < $colSize; $i++) {
-            if ($i < $colSize - 1) {
-                $sWhereSub .= $aColumns[$i] . " LIKE '%" . ($search) . "%' OR ";
-            } else {
-                $sWhereSub .= $aColumns[$i] . " LIKE '%" . ($search) . "%' ";
-            }
-        }
-        $sWhereSub .= ")";
-    }
-    $sWhere .= $sWhereSub;
-}
-
-
-
-
-$sQuery = "SELECT SQL_CALC_FOUND_ROWS api.api_track_id,api.requested_by,api.requested_on,api.number_of_records,api.request_type,api.test_type,api.api_url,api.api_params,api.facility_id,api.data_format FROM track_api_requests as api";
 
 if (!empty($sWhere)) {
-    $sWhere = ' WHERE ' . $sWhere;
-    $sQuery = $sQuery . ' ' . $sWhere;
+    $sWhere = implode(" AND ", $sWhere);
+    $sQuery = "$sQuery $sWhere";
 }
 
 if (!empty($sOrder) && $sOrder !== '') {
     $sOrder = preg_replace('/\s+/', ' ', $sOrder);
-    $sQuery = $sQuery . ' ORDER BY ' . $sOrder;
+    $sQuery = "$sQuery ORDER BY $sOrder";
 }
 
 if (isset($sLimit) && isset($sOffset)) {
-    $sQuery = $sQuery . ' LIMIT ' . $sOffset . ',' . $sLimit;
+    $sQuery = "$sQuery LIMIT $sOffset,$sLimit";
 }
-//die($sQuery);
-// echo $sQuery;
-$rResult = $db->rawQuery($sQuery);
-// print_r($rResult);
-/* Data set length after filtering */
 
-$aResultFilterTotal = $db->rawQueryOne("SELECT FOUND_ROWS() as `totalCount`");
-$iTotal = $iFilteredTotal = $aResultFilterTotal['totalCount'];
+[$rResult, $resultCount] = $db->getDataAndCount($sQuery);
 
 
-$output = array(
+$output = [
     "sEcho" => (int) $_POST['sEcho'],
-    "iTotalRecords" => $iTotal,
-    "iTotalDisplayRecords" => $iFilteredTotal,
+    "iTotalRecords" => $resultCount,
+    "iTotalDisplayRecords" => $resultCount,
     "aaData" => []
-);
+];
 
 foreach ($rResult as $aRow) {
     $row = [];
-    $row[] = ($aRow['requested_on']);
-    $row[] = ($aRow['number_of_records']);
-    $row[] = ($aRow['request_type']);
-    $row[] = ($aRow['test_type']);
-    $row[] = ($aRow['api_url']);
-    $row[] = ($aRow['data_format']);
+    $row[] = $aRow['requested_on'];
+    $row[] = $aRow['number_of_records'];
+    $row[] = $aRow['request_type'];
+    $row[] = $aRow['test_type'];
+    $row[] = $aRow['api_url'];
+    $row[] = $aRow['data_format'];
     $output['aaData'][] = $row;
 }
 
