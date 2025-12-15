91e3f0d6657ca800e78f447b4088fc3ec0edee73
Amit Dugar <amit@deforay.com>
2025-12-15 12:54:38 +0530

    Minor change in South Sudan results for Stamp feature

--- app/covid-19/results/pdf/result-pdf-ssudan.php
+++ app/covid-19/results/pdf/result-pdf-ssudan.php
@@ -189,7 +189,7 @@ if (!empty($requestResult)) {
             $result['labName'] = '';
         }
 
-        $stamp = UPLOAD_PATH . DIRECTORY_SEPARATOR . "facility-logo" . DIRECTORY_SEPARATOR . $result['lab_id'] . DIRECTORY_SEPARATOR . 'stamps' . DIRECTORY_SEPARATOR . 'hrl-stamp.png';
+        $stamp = UPLOAD_PATH . DIRECTORY_SEPARATOR . 'stamps' . DIRECTORY_SEPARATOR . 'hrl-stamp.png';
 
         if (MiscUtility::isImageValid($stamp)) {
             //$pdf->SetAlpha(0.6);
