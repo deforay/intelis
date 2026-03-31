5fa66de354acec429140822e6abcbe09dc250834
Amit Dugar <amit@deforay.com>
2026-03-31 17:44:42 +0530

    HIV VL Clinic Reports improvements

--- app/vl/program-management/vlDataQualityExportInExcel.php
+++ app/vl/program-management/vlDataQualityExportInExcel.php
@@ -4,28 +4,22 @@ use App\Utilities\MiscUtility;
 use App\Services\CommonService;
 use App\Services\DatabaseService;
 use App\Registries\ContainerRegistry;
-use PhpOffice\PhpSpreadsheet\IOFactory;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Style\Border;
-use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use OpenSpout\Common\Entity\Row;
+use OpenSpout\Writer\XLSX\Writer;
+
+ini_set('memory_limit', '512M');
+set_time_limit(300);
+ini_set('max_execution_time', 300);
 
 /** @var DatabaseService $db */
 $db = ContainerRegistry::get(DatabaseService::class);
 
 /** @var CommonService $general */
 $general = ContainerRegistry::get(CommonService::class);
-$sarr = $general->getSystemConfig();
 
-$arr = $general->getGlobalConfig();
 $key = (string) $general->getGlobalConfig('key');
 
-$delimiter = $arr['default_csv_delimiter'] ?? ',';
-$enclosure = $arr['default_csv_enclosure'] ?? '"';
-
-
 if (isset($_SESSION['vlIncompleteForm']) && trim((string) $_SESSION['vlIncompleteForm']) !== "") {
-
-     $output = [];
 
      $headings = ['Sample ID', 'Remote Sample ID', "Sample Collection Date", "Batch Code", "Unique ART No.", "Patient's Name", "Facility Name", "Province/State", "District/County", "Sample Type", "Result", "Status"];
      if ($general->isStandaloneInstance()) {
@@ -35,20 +29,20 @@ if (isset($_SESSION['vlIncompleteForm']) && trim((string) $_SESSION['vlIncomplet
           $headings = MiscUtility::removeMatchingElements($headings, ["Patient's Name"]);
      }
 
-     $resultSet = $db->rawQuery($_SESSION['vlIncompleteForm']);
+     $filename = TEMP_PATH . DIRECTORY_SEPARATOR . 'InteLIS-Data-Quality-report' . date('d-M-Y-H-i-s') . '.xlsx';
+
+     $writer = new Writer();
+     $writer->openToFile($filename);
+     $writer->addRow(Row::fromValues($headings));
+
+     $resultSet = $db->rawQueryGenerator($_SESSION['vlIncompleteForm']);
      foreach ($resultSet as $aRow) {
           $row = [];
-          //sample collecion date
           $sampleCollectionDate = '';
           if ($aRow['sample_collection_date'] != null && trim((string) $aRow['sample_collection_date']) !== '' && $aRow['sample_collection_date'] != '0000-00-00 00:00:00') {
                $expStr = explode(" ", (string) $aRow['sample_collection_date']);
-               $sampleCollectionDate =  date("d-m-Y", strtotime($expStr[0]));
+               $sampleCollectionDate = date("d-m-Y", strtotime($expStr[0]));
           }
-          // if($aRow['remote_sample']=='yes'){
-          //   $sampleId = $aRow['remote_sample_code'];
-          // }else{
-          //   $sampleId = $aRow['sample_code'];
-          // }
 
           $patientFname = $aRow['patient_first_name'] != '' ? $aRow['patient_first_name'] : '';
           $patientMname = $aRow['patient_middle_name'] != '' ? $aRow['patient_middle_name'] : '';
@@ -76,36 +70,10 @@ if (isset($_SESSION['vlIncompleteForm']) && trim((string) $_SESSION['vlIncomplet
           $row[] = ($aRow['sample_name']);
           $row[] = $aRow['result'];
           $row[] = ($aRow['status_name']);
-          $output[] = $row;
+
+          $writer->addRow(Row::fromValues($row));
      }
 
-     if (isset($_SESSION['vlIncompleteFormCount']) && $_SESSION['vlIncompleteFormCount'] > 50000) {
-          $fileName = TEMP_PATH . DIRECTORY_SEPARATOR . 'InteLIS-Data-Quality-report' . date('d-M-Y-H-i-s') . '.csv';
-          $fileName = MiscUtility::generateCsv($headings, $output, $fileName, $delimiter, $enclosure);
-          // we dont need the $output variable anymore
-          unset($output);
-          echo base64_encode((string) $fileName);
-     } else {
-
-          $excel = new Spreadsheet();
-          $sheet = $excel->getActiveSheet();
-
-
-          $styleArray = ['font' => ['bold' => true, 'size' => '13'], 'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER, 'vertical' => Alignment::VERTICAL_CENTER], 'borders' => ['outline' => ['style' => Border::BORDER_THIN]]];
-
-
-          $sheet->mergeCells('A1:AE1');
-          $sheet->getStyle('A3:M3')->applyFromArray($styleArray);
-
-          $sheet->fromArray($headings, null, 'A3');
-
-          foreach ($output as $rowNo => $rowData) {
-               $rRowCount = $rowNo + 4;
-               $sheet->fromArray($rowData, null, 'A' . $rRowCount);
-          }
-          $writer = IOFactory::createWriter($excel, IOFactory::READER_XLSX);
-          $filename = TEMP_PATH . DIRECTORY_SEPARATOR . 'InteLIS-Data-Quality-report' . date('d-M-Y-H-i-s') . '.xlsx';
-          $writer->save($filename);
-          echo urlencode(basename($filename));
-     }
+     $writer->close();
+     echo urlencode(basename($filename));
 }
