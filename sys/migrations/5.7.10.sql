5f587ec5e49fb5b202b900987459149d33b7871e
Amit Dugar <amit@deforay.com>
2026-08-18 19:32:59 +0530

    chore: bump version to 5.7.11

--- sys/migrations/5.7.10.sql
+++ sys/migrations/5.7.10.sql
@@ -1,8 +1,8 @@
--- Migration file for version 5.7.9
--- Created on 2026-08-18 14:50:14
+-- Migration file for version 5.7.10
+-- Created on 2026-08-18 14:59:50
 
 
-UPDATE `system_config` SET `value` = '5.7.9' WHERE `system_config`.`name` = 'sc_version';
+UPDATE `system_config` SET `value` = '5.7.10' WHERE `system_config`.`name` = 'sc_version';
 
 -- END OF VERSION --
 -- END OF VERSION --
